ecf2b799789eb6e3e45c08140001b776ce83b649
Lucas Toschini <lucas.toschini@live.fr>
2022-04-30 17:59:50 +0200

    routes + controllers + devise + navbar +footer

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,35 @@
 class OffersController < ApplicationController
+
+
+  def index
+    @offers = Offer.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+  def method_name
+
+  end
+  def offer_params
+    params.require(:offer).permit(:title, :description, :skill)
+  end
 end
