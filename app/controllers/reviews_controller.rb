0ed6c367c94e5def7ceb1c01e758a86e40587d77
Thomas Henriet <dabalx@gmail.com>
2022-05-10 21:22:11 +0200

    Create reviews for offers OK

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,25 @@
 class ReviewsController < ApplicationController
   def new
     @review = Review.new
+    @offer = Offer.find(params[:offer_id])
+    @review.offer = @offer
   end
 
   def create
     @review = Review.new(params_review)
     @offer = Offer.find(params[:offer_id])
     @review.offer = @offer
-    @review.save
+    @review.user = current_user
+    if @review.save
+      redirect_to(@offer)
+    else
+      render :new
+    end
   end
 
   private
 
   def params_review
-    params.require(:offer).permit(:rating, :content)
+    params.require(:review).permit(:rating, :content)
   end
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,5 @@
 class Offer < ApplicationRecord
+  has_many :reviews
   validates :title, presence: true
   validates :description, presence: true
   validates :description, length: { minimum: 20 }
