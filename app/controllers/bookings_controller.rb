c6605f1ab9e85fb9c832f832859dd221ddb830c4
Ondine Debienne <ondine.debienne@gmail.com>
2022-05-10 21:23:15 +0200

    booking feature and calendar

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,21 +1,25 @@
 class BookingsController < ApplicationController
+  # skip_before_action :verify_authenticity_token
+
   def new
+    @user = current_user
+    @offer = Offer.find(params[:offer_id])
     @booking = Booking.new
   end
 
   def create
-    @booking = Booking.new(params_booking)
+    @user = current_user
     @offer = Offer.find(params[:offer_id])
-    @user = User.find(params[:user_id])
+    @booking = Booking.new(params_booking)
     @booking.offer = @offer
     @booking.user = @user
     @booking.save
+    redirect_to user_path(@user)
   end
 
   private
 
   def params_booking
-    params.require(:booking).permit(:start_date, :end_date)
+    params.require(:booking).permit(:start_date, :end_date, :user_id, :offer_id)
   end
-
 end
