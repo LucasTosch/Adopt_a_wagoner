f83592872570e2c03704f4c1f4442cdaff5a9ca5
Ondine Debienne <ondine.debienne@gmail.com>
2022-05-12 20:59:14 +0200

    front, cleaning, search bar

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,6 +1,10 @@
 class OffersController < ApplicationController
   def index
-    @offers = Offer.all
+    if params[:query].present?
+      @offers = Offer.where("skill ILIKE ?", "%#{params[:query]}%")
+    else
+      @offers = Offer.all
+    end
   end
 
   def show
